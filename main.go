0911316b4e30b3cf154e95958670a1a38040df8c
mole828 <mrx800@163.com>
2023-03-29 17:39:27 +0800

    update

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"github.com/mole828/gin-websocket-demo/src/chatroom"
 	"github.com/mole828/gin-websocket-demo/src/chatroom/user"
+	"github.com/streadway/amqp"
 )
 
 type Msg struct {
@@ -14,14 +17,24 @@ type Msg struct {
 	Message string
 }
 
-func main() {
+func Port_already_in_use(port int) bool {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return true
+	} else {
+		ln.Close()
+		return false
+	}
+}
+
+func RunServer(ch *amqp.Channel) {
 	app := gin.New()
 	app.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"health": true,
 		})
 	})
-	chatroom := chatroom.New()
+	chatroom, _ := chatroom.New(ch)
 	app.GET("/ws", func(ctx *gin.Context) {
 		upGrande := websocket.Upgrader{
 			CheckOrigin: func(r *http.Request) bool {
@@ -29,12 +42,25 @@ func main() {
 			},
 			Subprotocols: []string{ctx.GetHeader("Sec-WebSocket-Protocol")},
 		}
-
 		conn, err := upGrande.Upgrade(ctx.Writer, ctx.Request, nil)
 		if err != nil {
 			return
 		}
 		chatroom.Join(user.New(conn))
 	})
-	app.Run(":8080")
+	port := 8080
+	for Port_already_in_use(port) {
+		port += 1
+	}
+	app.Run(fmt.Sprintf(":%d", port))
+}
+
+func main() {
+	conn, _ := amqp.Dial("amqp://golang:golangpass@www.moles.top:5672/")
+	defer conn.Close()
+
+	ch, _ := conn.Channel()
+	defer ch.Close()
+
+	RunServer(ch)
 }
--- src/chatroom/index.go
+++ src/chatroom/index.go
@@ -7,16 +7,12 @@ import (
 	"github.com/google/uuid"
 	"github.com/hashicorp/go-multierror"
 	"github.com/mole828/gin-websocket-demo/src/chatroom/user"
+	"github.com/streadway/amqp"
 )
 
 type Chatroom struct {
 	users map[string]*user.User
-}
-
-func New() *Chatroom {
-	return &Chatroom{
-		users: make(map[string]*user.User),
-	}
+	mqCh  *amqp.Channel
 }
 
 type Message struct {
@@ -24,7 +20,74 @@ type Message struct {
 	Value string
 }
 
-func (it *Chatroom) Send(message Message) error {
+func consumeMessages(ch *amqp.Channel, exchangeName string) (<-chan amqp.Delivery, error) {
+	// 创建一个新的队列
+	queue, err := ch.QueueDeclare(
+		"",    // 队列名称，由服务器随机生成
+		false, // 是否持久化
+		true,  // 是否自动删除
+		false, // 是否排他性
+		false, // 是否等待服务器响应
+		nil,   // 额外参数
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// 将队列绑定到 fanout 交换机上
+	err = ch.QueueBind(
+		queue.Name,   // 队列名称
+		"",           // 路由键
+		exchangeName, // 交换机名称
+		false,        // 是否等待服务器响应
+		nil,          // 额外参数
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// 消费队列中的消息
+	msgs, err := ch.Consume(
+		queue.Name, // 队列名称
+		"",         // 路由键
+		true,       // 是否自动确认消息
+		false,      // 是否独占
+		false,      // 是否等待服务器响应
+		false,      // 额外参数
+		nil,        // 额外参数
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return msgs, nil
+}
+
+var exchangeName string = "chat_broadcast"
+
+func New(ch *amqp.Channel) (*Chatroom, error) {
+	msgs, err := consumeMessages(ch, exchangeName)
+	if err != nil {
+		return nil, err
+	}
+	room := &Chatroom{
+		users: make(map[string]*user.User),
+		mqCh:  ch,
+	}
+	go func() {
+		for msg := range msgs {
+			var message *Message = &Message{}
+			err := json.Unmarshal(msg.Body, message)
+			if err != nil {
+				break
+			}
+			room.Send(message)
+		}
+	}()
+	return room, nil
+}
+
+func (it *Chatroom) Send(message *Message) error {
 	log.Printf("Send(%+v)\n", message)
 	data, err := json.Marshal(message)
 	if err != nil {
@@ -47,10 +110,25 @@ func (it *Chatroom) Join(user *user.User) {
 	})
 	go func() {
 		for msg := range user.Channel {
-			it.Send(Message{
+			message := Message{
 				From:  id,
 				Value: string(msg),
-			})
+			}
+			data, err := json.Marshal(message)
+			log.Printf("public: %s \n", message)
+			if err != nil {
+				break
+			}
+			it.mqCh.Publish(
+				exchangeName,
+				"",
+				false,
+				false,
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        data,
+				},
+			)
 		}
 	}()
 }
